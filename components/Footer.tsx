316aec8976728f8dd00355b3890056e27b632036
Shota Sakumoto <info@foodit.co.jp>
2025-05-19 22:35:19 +0900

    fix: update footer copyright year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { Box, Text } from '@chakra-ui/react';
 
 const Footer = (): JSX.Element => (
   <Box as="footer" bg="gray.100" py={2} textAlign="center">
-    <Text fontSize="sm">© 2023 Type Puzzle</Text>
+    <Text fontSize="sm">© 2025 Type Puzzle</Text>
   </Box>
 );
 
